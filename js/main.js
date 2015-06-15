65991ebd35fd6584858b5b948b2322cd3cbca44d
dis-zephyro <zephyro@yandex.ru>
2015-06-16 02:52:09 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -124,3 +124,29 @@ $.validate({
     language : 'russian'
 });
 
+
+// menu --------
+
+$(document).ready(function() {
+// Указываем переменные
+    var accordion_head = $('.side-nav  li > a'),
+        accordion_body = $('.side-nav li > .sub-menu');
+
+// Функция клика
+    accordion_head.on('click', function(event) {
+// Скрывает открытый раздел по повторному клику
+        event.preventDefault();
+        if ($(this).attr('class') == 'active'){
+            accordion_body.slideUp('normal')
+            $(this).removeClass('active');
+            return false;
+        }
+// Открывает следующий, скрывая открытый
+        if ($(this).attr('class') != 'active'){
+            accordion_body.slideUp('normal');
+            $(this).next().stop(true,true).slideToggle('normal');
+            accordion_head.removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+});
